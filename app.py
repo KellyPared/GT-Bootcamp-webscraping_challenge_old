b9672240a00583133626a337838461b7ee286780
KellyPared <kellschu333@gmail.com>
2023-03-15 15:53:45 -0400

    files

--- app.py
+++ app.py
@@ -4,20 +4,33 @@ import scrape_mars
 
 app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
-@app.route('/')
-def query_database():
-    '''This function will query database and pass mars data into HTML'''
-
-@app.route('/scrape')
-def scrape():
-    
-    return #dictionary
+# Or set inline
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/phone_app")
 
 
+@app.route("/")
+def index():
+    # find one document from our mongo db and return it.
+    mars_latest = mongo.db.mars_latest.find_one()
+    # pass that listing to render_template
+    return render_template("index.html", mars_latest=mars_latest)
+
+# set our path to /scrape
+@app.route("/scrape")
+def scraper():
+    # create a listings database
+    mars = mongo.db.mars_latest
+    # call the scrape function in our scrape_phone file. This will scrape and save to mongo.
+    mars_data = scrape_mars.scrape()
+    # update our listings with the data that is being scraped.
+    mars.update({}, mars_data, upsert=True)
+    # return a message to our page so we know it was successful.
+    return redirect("/", code=302)
 
 
-
-
+if __name__ == "__main__":
+    app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -20,6 +20,39 @@ def open_browser():
     browser = Browser('chrome')
     return browser
 
+def lastest_news():
+    
+    return title, paragraph
+
+
+def featured_mars():
+    return url
+
+
+def mars_table():
+    url = 'https://galaxyfacts-mars.com/'
+    all_tables = pd.read_html(url)
+    tb = mars_table.to_html(classes = 'table')
+    return tb
+
+def hemispheres():
+    list_hemis = []
+    list_hemis.append({title: #text,
+     img_url: #url
+     })
+
+    return [#list of dictionaries]
+
 def scrape():
-    ''' This scrapes the page.'''
+    ''' This scrapes the page. main function'''
     browser = open_browser()
+    title, paragraph = lastest_news()
+    image_url = featured_mars()
+
+    
+    mars_dictionary = {'news_title': title,
+                       'news_paragraph': paragraph,
+                       'featured_image': image_url,
+                        'facts': mars_table(),
+                        'hemispheres': []
+                       }
