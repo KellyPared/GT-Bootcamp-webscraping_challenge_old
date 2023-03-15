cd4f409227f7cb97123dd71869b8afb6aabb8f08
KellyPared <kellschu333@gmail.com>
2023-03-15 15:00:03 -0400

    index

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-from scrape_mars import scrape
-from flask import Flask
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
 
 app = Flask(__name__)
 
-
-@app.route('/scrape')
-def call_scrape():
-    scrape()
-    return #dictionary
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 @app.route('/')
 def query_database():
-    '''This function will query daatbase and pass mars data into HTML'''
+    '''This function will query database and pass mars data into HTML'''
 
-
+@app.route('/scrape')
+def scrape():
+    
+    return #dictionary
 
 
 
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,5 +1,25 @@
+# Import Splinter and BeautifulSoup
+from splinter import Browser
+from bs4 import BeautifulSoup as bs4
+import requests
+import pandas as pd
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from pprint import pprint 
+import csv
+import os
+import sys
+import time
+os.path.dirname(sys.executable)
 
-
+def open_browser():
+    '''This launches browser.'''
+    #browser = webdriver.Chrome()
+    #global browser
+    browser = Browser('chrome')
+    return browser
 
 def scrape():
-    pass
+    ''' This scrapes the page.'''
+    browser = open_browser()
